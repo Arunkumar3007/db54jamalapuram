a8649674af6948c220277b836a9179a1b2ffe349
Arunkumar3007 <89620964+Arunkumar3007@users.noreply.github.com>
2021-11-19 11:25:46 -0600

    added BUTTONS

--- app.js
+++ app.js
@@ -23,61 +23,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// passport config
+// Use the existing connection
+// The Account model
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mobile', mobileRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-const connectionString = process.env.MONGO_CON
-
-mongoose = require('mongoose');
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// We can seed the collection if needed on server start
-async function recreateDB(){
-  // Delete everything
-  await mobile.deleteMany();
-
-
-  var results = [{"name":"Motrola","type":'Android',"cost":300},
-                 {"name":"IPHONE","type":'IOS',"cost":1200},
-                 {"name":"Samsung", "type":'Android',"cost":800}]
-
- for(i in results){
-   let instance = new  mobile({name: results[i]["name"], type: results[i]["type"], cost:results[i]["cost"]});
-   instance.save( function(err,doc) {
-     if(err) return console.error(err);
-     console.log("object added.")
-     });
- }
-
- }
-
- let reseed = true;
- if (reseed) { recreateDB();}
-
-module.exports = app;
-
-var db = mongoose.connection; 
- 
-//Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
-db.once("open", function(){ 
-console.log("Connection to DB succeeded")}); 
--- routes/mobile.js
+++ routes/mobile.js
@@ -3,5 +3,7 @@ const mobile_controller = require('../controllers/mobile');
 var router = express.Router(); 
  
 // router.get('/delete', mobile_controller.mobile_delete_Page);
-router.get('/detail', mobile_controller.mobile_view_one_Page);
-module.exports = router; 
+router.get('/', mobile_controller.mobile_view_all_Page);
+router.get('/update', mobile_controller.mobile_update_Page);
+
+module.exports = router;
