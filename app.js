7bdbc133681bd4ead9770b2ba99635677f9f6ddb
Arunkumar3007 <89620964+Arunkumar3007@users.noreply.github.com>
2021-12-01 15:46:17 -0600

    added router

--- app.js
+++ app.js
@@ -57,3 +57,58 @@ passport.deserializeUser(Account.deserializeUser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mobile', mobileRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const connectionString = process.env.MONGO_CON
+
+mongoose = require('mongoose');
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+  // Delete everything
+  await mobile.deleteMany();
+
+
+  var results = [{"name":"Motrola","type":'Android',"cost":300},
+                 {"name":"IPHONE","type":'IOS',"cost":1200},
+                 {"name":"Samsung", "type":'Android',"cost":800}]
+
+ for(i in results){
+   let instance = new  mobile({name: results[i]["name"], type: results[i]["type"], cost:results[i]["cost"]});
+   instance.save( function(err,doc) {
+     if(err) return console.error(err);
+     console.log("object added.")
+     });
+ }
+
+ }
+
+ let reseed = true;
+ if (reseed) { recreateDB();}
+
+module.exports = app;
+
+var db = mongoose.connection; 
+
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+console.log("Connection to DB succeeded")}); 
